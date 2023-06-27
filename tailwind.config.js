8cc7b811716710c1a3c203e40bb58a5d6e3f94b4
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2023-06-27 16:58:36 +0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#8C82FC",
+        navlink: "#394360",
+        body: "#222831",
+        title: "#22293E",
+        secondary: "#A2B3BF",
+        accent: "#44E4C9",
+        "light-purple": "#F8F8FF",
       },
     },
   },
   plugins: [],
-}
+};
